2f7bf8eb53453953291ba9194cb943f21c566a22
Berlianapd <pancadewiiii@gmail.com>
2026-03-08 02:44:24 +0700

    5053231028_Berliana Panca_TugasTailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#1E40AF',
+        accent: '#7C3AED',
+        background: '#F8FAFC',
+      },
+    },
+  },
+  plugins: [],
+}
